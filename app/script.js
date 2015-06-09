811375f146c5492355b68716392d8a230364fcff
Callum Macrae <callum@macr.ae>
2015-06-09 22:27:43 +0100

    added more randomness holy crap

--- app/script.js
+++ app/script.js
@@ -6,13 +6,14 @@ const height = 640;
 const svg = d3.select('svg');
 
 const circles = svg.selectAll('circle')
-	.data(randomNumbers(50, width));
+	.data(randomNumbers(600, width));
 
 const circleEnter = circles.enter().append('circle');
 
 circleEnter.attr('cy', () => Math.random() * (height - 60) + 30);
-circleEnter.attr('cx', (d) => d);
-circleEnter.attr('r', (d) => Math.sqrt(d));
+circleEnter.attr('cx', (d) => biasLeft(d));
+circleEnter.attr('r', (d) => Math.sqrt(biasLeft(d)));
+circleEnter.attr('opacity', () => (Math.random() + 0.5) / 6);
 
 /**
  * Returns an array of numbers.
@@ -23,4 +24,26 @@ circleEnter.attr('r', (d) => Math.sqrt(d));
  */
 function randomNumbers(n=1, max=100) {
 	return n === 0 ? [] : [...randomNumbers(n - 1, max), Math.random() * max];
+}
+
+/**
+ * Biases a number on a spectrum towards 0 sine-ly.
+ *
+ * @param {number} x The number.
+ * @param {number} max The maximum number in the spectrum.
+ * @returns {number} The new number.
+ */
+function biasLeft(x, max=width) {
+	return (2 * max * Math.asin(x / max)) / Math.PI;
+}
+
+/**
+ * Same as biasLeft but biases the number towards the max.
+ *
+ * @param {number} x The number.
+ * @param {number} max The maximum number in the spectrum.
+ * @returns {number} The new number.
+ */
+function biasRight(x, max=width) {
+	return Math.sin((x / max * Math.PI) / 2) * max;
 }
