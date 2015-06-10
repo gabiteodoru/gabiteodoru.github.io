d0b8bdb084c3251e20a3d5454166623fbbf1f1ee
unknown <gabiteodoru@gmail.com>
2015-06-10 10:17:49 +0100

    Initial commit

--- app/script.js
+++ app/script.js
@@ -7,11 +7,11 @@ const HEIGHT = 640;
 const svg = d3.select('svg');
 
 const circles = svg.selectAll('circle')
-	.data(randomNumbers(600, WIDTH));
+	.data(randomNumbers(3600, WIDTH));
 
 const circleEnter = circles.enter().append('circle');
 
 circleEnter.attr('cy', () => Math.random() * (HEIGHT - 60) + 30);
 circleEnter.attr('cx', (d) => biasLeft(d, WIDTH));
-circleEnter.attr('r', (d) => Math.sqrt(biasLeft(d, WIDTH)));
+circleEnter.attr('r', (d) => Math.exp(3*d/WIDTH));
 circleEnter.attr('opacity', () => (Math.random() + 0.5) / 6);
